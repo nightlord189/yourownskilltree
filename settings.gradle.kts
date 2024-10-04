c59688c80cee3a5051e728c703d4326f7efd3a0b
Alexey Buravov <aburavov189@gmail.com>
2024-10-05 03:03:55 +0500

    new project structure

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,10 +10,5 @@ plugins {
 }
 rootProject.name = "yourownskilltree"
 
-include("m1l1-first")
-include("m1l2-basic")
-include("m1l3-func")
-include("m1l4-oop")
-include("m2l1-dsl")
-include("m2l2-coroutines")
-include("m2l3-flows")
+includeBuild("lessons")
+includeBuild("yourownskilltree-be")
