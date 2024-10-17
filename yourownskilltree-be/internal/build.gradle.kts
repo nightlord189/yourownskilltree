fa1930acdd3fd1710e7d2ed083db20315ec975de
Alexey Buravov <aburavov189@gmail.com>
2024-10-17 22:59:36 +0500

    m3 homework: requirements and architecture (#3)
    * m3 homework
    
    * change readme
    
    * drawio file

--- yourownskilltree-be/internal/build.gradle.kts
+++ yourownskilltree-be/internal/build.gradle.kts
@@ -1,3 +1,15 @@
 plugins {
     id("build-jvm")
+    kotlin("jvm") version "1.9.23"
+    application
+}
+
+application {
+    mainClass.set("org.aburavov.yourownskilltree.backend.internal.MainKt")
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "org.aburavov.yourownskilltree.backend.internal.MainKt"
+    }
 }
--- yourownskilltree-be/internal/src/main/kotlin/main.kt
+++ yourownskilltree-be/internal/src/main/kotlin/main.kt
@@ -1,3 +1,5 @@
+package org.aburavov.yourownskilltree.backend.internal
+
 fun main() {
     println("Hello from yourownskilltree-internal")
 }
